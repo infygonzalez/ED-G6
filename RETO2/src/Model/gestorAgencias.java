118445e1b66d22cccaa78d0117124fd2b97cb0ce
morci <morci@Alvaro>
2025-01-23 19:28:46 +0100

    gestorAgencias actualizado

--- RETO2/src/Model/gestorAgencias.java
+++ RETO2/src/Model/gestorAgencias.java
@@ -6,9 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import Model.Agencia.*;
-import Model.DBUtils.*;
+
 
 
 public class gestorAgencias {
