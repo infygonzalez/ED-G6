a78a8225bdb8b0d02a4df8e6d86c0361c4cf79e5
in1dm3-a <in1dm3-a@HZ352030.elorrieta.int>
2025-01-23 09:39:58 +0100

    queries sql

--- RETO2/src/Model/SQLQueries.java
+++ RETO2/src/Model/SQLQueries.java
@@ -1,5 +1,12 @@
 package Model;
-
+/*
+ * faltan cosas
+ */
 public class SQLQueries {
-
+	//SELECTS
+	public static final String SELECT_NOMBRE_AGENCIAS = "select nombre from Agencias";
+	//INSERTS
+	public static final String INSERT_AGENCIAS = "insert into Agencias values ('";
+	public static final String SEPARATOR = "', '";
+	public static final String END_BLOCK = "')";
 }
