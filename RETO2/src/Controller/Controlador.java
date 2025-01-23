53b31c791bdf6889a6ed659d5a0721a90a2179e8
in1dm3-a <in1dm3-a@HZ352030.elorrieta.int>
2025-01-23 09:39:39 +0100

    controlador

--- RETO2/src/Controller/Controlador.java
+++ RETO2/src/Controller/Controlador.java
@@ -1,12 +1,15 @@
 package Controller;
-
+import Model.gestorAgencias;
+import Model.Agencia;
 public class Controlador {
 
-	public void comprobarAgencia() {
-		
+	public void comprobarAgencia(Agencia Agencia) {
+		gestorAgencias gestorAgencias = new gestorAgencias();
+		gestorAgencias.comprobarAgencia(Agencia);
 	}
 	public void crearAgencia() {
-		
+		gestorAgencias gestorAgencias = new gestorAgencias();
+		gestorAgencias.crearAgencia();
 	}
 	
 }
