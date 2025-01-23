58d6f2feae4f3f2c37a72861dc7fb3917f2835cb
in1dm3-a <in1dm3-a@HZ352030.elorrieta.int>
2025-01-23 08:56:31 +0100

    Clase gestor

--- RETO2/src/Model/gestorAgencias.java
+++ RETO2/src/Model/gestorAgencias.java
@@ -7,7 +7,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import Model.Agencia;
+import Model.DBUtils.*;
+
 
 public class gestorAgencias {
-
+	Connection conexion = null;
+	Statement sentencia = null;
+	
+	public void comprobarAgencia(Agencia agencia) {
+		//Dan errores porque hay que meterlo entre try catch//
+		Class.forName(DBUtils.DRIVER);
+		conexion = DriverManager.getConnection(DBUtils.URL, DBUtils.USER, DBUtils.PASSWORD);
+		sentencia = conexion.createStatement();
+	}
 }
