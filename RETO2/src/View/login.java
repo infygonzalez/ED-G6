2ee67ab8a110e1f4592855d325ba01074ae92706
morci <morci@Alvaro>
2025-01-23 17:58:33 +0100

    primera version de la pantalla de login

--- RETO2/src/View/login.java
+++ RETO2/src/View/login.java
@@ -1,16 +1,62 @@
 package View;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class login extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField txtUsuario;
+	private JPasswordField txtContraseña;
 
 	/**
 	 * Create the panel.
 	 */
 	public login() {
+		setForeground(new Color(192, 192, 192));
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Iniciar sesion");
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+		lblNewLabel.setBounds(162, 5, 126, 28);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_Usuario = new JLabel("Usuario:");
+		lblNewLabel_Usuario.setFont(new Font("Verdana", Font.PLAIN, 11));
+		lblNewLabel_Usuario.setBounds(67, 71, 55, 14);
+		add(lblNewLabel_Usuario);
+		
+		JLabel lblNewLabel_Contraseña = new JLabel("Contraseña:");
+		lblNewLabel_Contraseña.setFont(new Font("Verdana", Font.PLAIN, 11));
+		lblNewLabel_Contraseña.setBounds(67, 120, 81, 14);
+		add(lblNewLabel_Contraseña);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setFont(new Font("Verdana", Font.PLAIN, 11));
+		txtUsuario.setBounds(197, 68, 160, 20);
+		add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		txtContraseña = new JPasswordField();
+		txtContraseña.setBounds(195, 118, 162, 20);
+		add(txtContraseña);
+		
+		JButton btnValidar = new JButton("Validar");
+		btnValidar.setFont(new Font("Verdana", Font.PLAIN, 11));
+		btnValidar.setBackground(Color.GRAY);
+		btnValidar.setBounds(67, 181, 89, 23);
+		add(btnValidar);
+		
+		JButton btnCrearUsuario = new JButton("Crear usuario");
+		btnCrearUsuario.setFont(new Font("Verdana", Font.PLAIN, 11));
+		btnCrearUsuario.setBackground(Color.GRAY);
+		btnCrearUsuario.setBounds(243, 181, 114, 23);
+		add(btnCrearUsuario);
 
 	}
-
 }
