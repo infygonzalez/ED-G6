833462dbc63d41ba5ba309957520efc3fbbc45da
in1dm3-a <in1dm3-a@HZ352024.elorrieta.int>
2025-02-07 13:08:51 +0100

    test

--- Reto2IbaiMiguelAlvaro/src/Model/gestorAgenciasTest.java
+++ Reto2IbaiMiguelAlvaro/src/Model/gestorAgenciasTest.java
@@ -13,46 +13,7 @@ public class gestorAgenciasTest {
 
 	@Test
 	public void testComprobarAgencia(Agencia agencia) {
-		Connection conexion = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		String verificar= SQLQueries.SELECT_CONTRA_AGENCIAS;//otra vez lo mismo
 		
-		try {
-			Class.forName(DBUtils.DRIVER);
-			conexion = DBUtils.getConexion();//Iniciamos la conexion
-			// Verificar si el usuario y la contraseña coincide
-			stmt = conexion.prepareStatement(verificar);
-			stmt.setString(1, agencia.getNombre());
-			stmt.setString(2, agencia.getContra());
-			rs = stmt.executeQuery();
-			if(rs.next()) {
-				agencia = new Agencia();
-				agencia.setID(rs.getInt("id_agencia"));
-				agencia.setColor(rs.getString("color_marca"));
-				agencia.setNombre(rs.getString("nombre"));
-				agencia.setNumeroEmpleados(rs.getString("numero_empleados"));
-				agencia.setLogo(rs.getString("logo"));
-				agencia.setContra(rs.getString("contrasena"));
-				agencia.setTipoAgencia(rs.getString("tipo_agencia"));
-
-
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-            // Cerrar recursos
-            try {
-                if (rs != null) rs.close();
-                if (stmt != null) stmt.close();
-                if (conexion != null) conexion.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-		}
 		fail("Not yet implemented");
 	}
 
